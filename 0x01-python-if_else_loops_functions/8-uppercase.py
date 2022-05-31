6ebbd426b504eedd9930ba61fe699c17cf0a0d38
kimmy12-web <kimmymubiru12@gmail.com>
2022-05-31 21:16:17 +0300

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    print("{}".format(number % 10), end="")
-    return (number % 10)
+def uppercase(str):
+    result = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print("{}".format(result))
